c4326a0ca3475c1b425c8c038a14bfa5cedcecb8
sbrb <info.sbriju@gmail.com>
2023-01-19 19:44:29 +0530

    s

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,97 +1,37 @@
-const axios = require('axios')
-
-//getStates
-const getStates = async (req, res) => {
+const axios = require('axios');
+//getAllMemes
+const getAllMemes = async (req, res) => {
   try {
     const options = {
       method: 'get',
-      url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
+      url: 'https://api.imgflip.com/get_memes'
     }
-    const result = await axios(options)
-    const data = result.data
-    res.status(500).send({ msg: data, status: true })
-  }
-  catch (err) {
-    res.status(500).send({ msg: err.massage })
-  }
-}
-//getDistricts
-const getDistricts = async (req, res) => {
-  try {
-    const stateId = req.params.stateId;
-    const options = {
-      method: 'get',
-      url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${stateId}`
-    }
-    let result = await axios(options);
+    const result = await axios(options);
+    console.log(options);
     const data = result.data
     res.status(200).send({ msg: data, status: true })
-  }
-  catch (err) {
-    res.status(500).send({ msg: err.massage })
-  }
-}
-//getByPin
-const getByPin = async (req, res) => {
-  try {
-    const pin = req.query.pincode
-    const date = req.query.date
-    const options = {
-      method: 'get',
-      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
-    }
-    let result = await axios(options);
-    const data = result.data
-    res.status(200).send({ msg: data, status: true })
-  }
-  catch (err) {
-    res.status(500).send({ msg: err.massage })
-  }
-}
-//getOtp
-const getOtp = async (req, res) => {
-  try {
-    var options = {
-      method: 'post',
-      url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
-    }
-    const result = await axios(options)
-    res.status(200).send({ msg: result.data })
   }
   catch (err) {
     res.status(500).send({ msg: err.message })
   }
 }
-//confirmOTP
-const confirmOTP = async (req, res) => {
+//createMeme
+const createMeme = async (req, res) => {
   try {
+    const template_id = req.query.template_id;
+    const text0 = req.query.text0
+    const text1 = req.query.text1
+    const username = req.query.username;
+    const password = req.query.password;
     const options = {
       method: 'post',
-      url: `https://cdn-api.co-vin.in/api/v2/auth/public//v2/auth/public/confirmOTP`,
+      url: `https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`
     }
     const result = await axios(options)
-    res.status(200).send({ msg: result.data })
-  }
-  catch (err) {
-    res.status(500).send({ msg: err.message })
-  }
-}
-//Assignment
-// districtId
-const getDistrictId = async (req, res) => {
-  try {
-    const districtId = req.query.districtId;
-    const date = req.query.date;
-    const option = {
-      method: 'get',
-      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${date}`
-    }
-    const result = await axios(option)
-    res.status(200).send({ msg: result.data });
+    res.status(200).send({ msg: result.data, status: true })
   }
   catch (err) {
     res.status(500).send({ error: err.message })
   }
 }
-
-module.exports = { getStates, getDistricts, getByPin, getOtp, confirmOTP,getDistrictId}
+module.exports = { getAllMemes, createMeme }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/controller');
+const controller = require("../controllers/controller")
 
-router.get('/getState', controller.getStates);
-router.get('/getDistricts/:stateId', controller.getDistricts);
-router.get('/getByPin', controller.getByPin);
-router.post('/getOtp', controller.getOtp);
-router.post('/confirmOTP', controller.confirmOTP);
-//Assignment
-router.get('/getDistrictId', controller.getDistrictId);
+router.get("/getAllMemes", controller.getAllMemes);
+router.post("/createMeme", controller.createMeme);
 
 module.exports = router;
