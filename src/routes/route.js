1acdb0d838d21ed7f8574ed75ba39a11fbcae4f9
sbrb <info.sbriju@gmail.com>
2023-01-19 19:47:18 +0530

    s

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const controller = require("../controllers/controller")
+const controller = require('../controllers/controller');
 
-router.get("/getAllMemes", controller.getAllMemes);
-router.post("/createMeme", controller.createMeme);
+router.get('/getAllMemes', controller.getAllMemes);
+router.post('/createMeme', controller.createMeme);
 
 module.exports = router;
